1dce7f356aea8bd7fff62f9ad6dcdcae2bc7967f
AidanDennehy <44627373+Dracfo@users.noreply.github.com>
2020-11-06 08:39:58 -0500

    Working watchlist

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django import forms
+from django.forms import MultiWidget, TextInput
 
+
+class NewListingForm(forms.Form):
+    title = forms.CharField(max_length=64, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    description = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    price = forms.DecimalField(max_digits=6, decimal_places=2, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    image = forms.URLField(label="Image URL", required=False, widget=forms.TextInput(attrs={'class': 'form-control'}))
 
 class User(AbstractUser):
     pass
@@ -8,11 +16,29 @@ class User(AbstractUser):
 
 class Auction_listing(models.Model):
     id = models.AutoField(primary_key = True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    starting_bid = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.URLField(blank=True)
+    is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Listing {self.id}: {self.title}, selling for ${self.starting_bid}"
 
 
 class Bid(models.Model):
     id = models.AutoField(primary_key = True)
+    auction_id = models.ForeignKey(Auction_listing, on_delete=models.CASCADE, related_name="bids")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+    bid_sum = models.DecimalField(max_digits=6, decimal_places=2)
 
 
 class Comment(models.Model):
     id = models.AutoField(primary_key = True)
+
+
+class Watchlist(models.Model):
+    id = models.AutoField(primary_key = True)
+    auction_id = models.ForeignKey(Auction_listing, on_delete=models.CASCADE, related_name="watchlist")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
--- auctions/views.py
+++ auctions/views.py
@@ -1,15 +1,95 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from .models import User
+from .models import User, Auction_listing, Watchlist, NewListingForm
 
 
 def index(request):
-    return render(request, "auctions/index.html")
+    listings = Auction_listing.objects.filter(is_done=False)
 
+    print(listings)
+    
+    return render(request, "auctions/index.html", {
+        'listings': listings
+    })
+
+@login_required
+def create_listing(request):
+    if request.method == "POST":
+        # if this is a POST request, process the form data
+        form = NewListingForm(request.POST)
+
+        # check whether it's valid:
+        if form.is_valid():
+            
+            # process the data in form.cleaned_data as required
+            title = form.cleaned_data["title"]
+            description = form.cleaned_data["description"]
+            price = form.cleaned_data["price"]
+            image = form.cleaned_data["image"]
+
+            # Add the listing to the listings database
+            new = Auction_listing(created_by=request.user, title=title, description=description, starting_bid=price, image=image)
+            new.save()
+        return redirect("/")
+
+    else:
+        return render(request, "auctions/create_listing.html", {
+            'page_form': NewListingForm()
+        })
+
+
+def listing(request, id):
+    listing = Auction_listing.objects.get(id = id)
+    
+    return render(request, "auctions/listing.html", {
+        'listing': listing
+    })
+
+
+@login_required
+def watchlist(request):
+    # Get all the listing ids current user is watching
+    listing_ids = Watchlist.objects.filter(user_id=request.user).values('auction_id')
+    count = Watchlist.objects.filter(user_id=request.user).values('auction_id').count()
+    
+    # Fill a list of all the listings being watch
+    auction_listings = []
+    for i in range(count):
+        auction_listings.append(Auction_listing.objects.filter(id=listing_ids[i]["auction_id"]))
+    
+    listings = []
+    for i in range(count):
+        for listing in auction_listings[i]:
+            listings.append(auction_listings[i][0])
+            print(listings)
+    # Return html page with all listings being watched by user
+    return render(request, "auctions/watchlist.html", {
+        'listings': listings
+    })
+
+
+@login_required
+def add_watchlist(request, id):
+    # Add listing to the watchlist
+    add_listing = Watchlist(auction_id=Auction_listing.objects.get(id = id), user_id=request.user)
+    add_listing.save()
+
+    # Redirect to the watchlist
+    return redirect("/watchlist")
+
+
+@login_required
+def remove_watchlist(request, id):
+    # Add listing to the watchlist
+    Watchlist.objects.filter(auction_id=Auction_listing.objects.get(id = id), user_id=request.user).delete()
+
+    # Redirect to the watchlist
+    return redirect("/watchlist")
 
 def login_view(request):
     if request.method == "POST":
